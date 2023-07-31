3c21353e8c8fb3ffab332dc3fb9ce7a825574cfa
Layne <saxalayne@gmail.com>
2023-07-31 10:28:09 -0600

    added user and binder models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,21 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+class User(db.model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+class Binder(db.model, SerializerMixin):
+    __tablename__ = 'binders'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+
+
