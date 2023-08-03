707b0831e85a50f60bc674b97402da9e27c1a3d3
Layne <saxalayne@gmail.com>
2023-08-03 12:39:22 -0600

    updated models and added seed file

--- server/models.py
+++ server/models.py
@@ -2,8 +2,19 @@ from ipdb import set_trace
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
 
 db = SQLAlchemy()
+
+class Binder(db.model, SerializerMixin):
+    __tablename__ = 'binders'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    serialize_rules = ('-created_at', '-updated_at', '-user_id')
 
 class User(db.model, SerializerMixin):
     __tablename__ = 'users'
@@ -12,13 +23,23 @@ class User(db.model, SerializerMixin):
     email = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    _password_hash = db.Column(db.String)     
 
-class Binder(db.model, SerializerMixin):
-    __tablename__ = 'binders'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-              
+    serialize_rules = ("-_password_hash","-admin", "-created_at", "-updated_at")     
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash 
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        from app import bcrypt
+        pw_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = pw_hash.decode("utf-8")
+
+    def authenticate(self, password):
+        from app import bcrypt
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+
 
 
