55dc221cb306c83fc6fd301919e12615b6398a64
Layne <saxalayne@gmail.com>
2023-08-03 16:30:25 -0600

    added binders to seed.py

--- server/seed.py
+++ server/seed.py
@@ -5,7 +5,19 @@ with app.app_context():
     User.query.delete()
     Binder.query.delete()
 
+    binders = []
+
     b1 = Binder(name="1st Edition Base set")
-    
-    db.session.add(b1)
+    binders.append(b1)
+
+    b2 = Binder(name="Paldea Evolved Collection")
+    binders.append(b2)
+
+    b3 = Binder(name="Crown Zenith Rares")
+    binders.append(b3)
+
+    b4 = Binder(name="My random cards")
+    binders.append(b4)
+
+    db.session.add_all(binders)
     db.session.commit()
