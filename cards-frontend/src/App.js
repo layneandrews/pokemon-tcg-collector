93d83aedfffdfc493e409298d6aa696c0e5535a8
Layne <saxalayne@gmail.com>
2023-08-06 15:24:50 -0600

    added home component

--- cards-frontend/src/App.js
+++ cards-frontend/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import { Route, Routes, useNavigate } from "react-router-dom"
+// import { createGlobalStyle } from "styled-components"
+// import { useEffect, useState } from "react"
+// import Home from "./components/Home"
+// import ProductionForm from "./components/ProductionForm"
+// import ProductionEdit from "./components/ProductionEdit"
+// import Navigation from "./components/Navigation"
+// import ProductionDetail from "./components/ProductionDetail"
+// import NotFound from "./components/NotFound"
+// import Authentication from "./components/Authentication"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+    </Routes>
   );
 }
 
---
+++ cards-frontend/src/components/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            <h1>Hi this is the home component</h1>
+        </div>
+    )
+}
+
+export default Home
