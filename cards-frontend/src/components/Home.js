d4e40ea37dbd869d3c0033103f7b27c4cb7fe93b
Layne <saxalayne@gmail.com>
2023-08-07 18:05:29 -0600

    added frontend components

--- cards-frontend/src/components/Home.js
+++ cards-frontend/src/components/Home.js
@@ -1,7 +1,24 @@
 function Home(){
+
+    const handleSub = () => {
+        fetch("https://api.pokemontcg.io/v2/cards", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(),
+        })
+          .then((r) => r.json())
+          .then(data => {
+            console.log(data)
+          });
+      };
+    
     return(
         <div>
             <h1>Hi this is the home component</h1>
+            <form action="/search" method="GET">
+            <input type="text" id="search" name="q" placeholder="Search for a Pokemon card"/>
+            <button onSubmit={handleSub} type="submit">Search</button>
+            </form>
         </div>
     )
 }
