739cb2b3ae02161a61619179ea228be815c66384
Layne <saxalayne@gmail.com>
2023-08-18 16:05:48 -0600

    add to front end

--- cards-frontend/src/components/Home.js
+++ cards-frontend/src/components/Home.js
@@ -1,27 +1,22 @@
-function Home(){
+function Home() {
 
-    const handleSub = () => {
-        
-        fetch("https://api.pokemontcg.io/v2/cards", {
-          method: "GET",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify,
-        })
-          .then((r) => r.json())
-          .then(data => {
-            
-            console.log(data)
-          });
-      };
+  const handleSub = (e) => {
+    e.preventDefault()
+    fetch("https://api.pokemontcg.io/v2/cards/xy1-1")
+      .then((r) => r.json())
+      .then(data => {
+        console.log(data);
+    });
+  } 
     
-    return(
-        <div>
-            <h1>Hi this is the home component</h1>
-            <form action="/search" method="GET">
-            <input type="text" id="search" name="q" placeholder="Search for a Pokemon card"/>
-            <button onSubmit={handleSub} type="submit">Search</button>
-            </form>
-        </div>
+  return(
+    <div>
+      <h1>Hi this is the home component</h1>
+        <form onSubmit={handleSub}>
+          <input type="text" placeholder="Search for a Pokemon card"/>
+          <button type="submit">Search</button>
+        </form>
+    </div>
     )
 }
 
