5b3b148ba8736644e33eac8a2e78d72b4f1ce58d
Neal Hansen <n.hans3n@gmail.com>
2023-09-03 14:26:40 -0600

    actually commit changes

--- cards-frontend/src/components/Home.js
+++ cards-frontend/src/components/Home.js
@@ -1,23 +1,67 @@
-function Home() {
+import { useState } from 'react';
 
+function Home() {
   const handleSub = (e) => {
     e.preventDefault()
-    fetch("https://api.pokemontcg.io/v2/cards/xy1-1")
+    const cardId = e.target[0].value
+    fetch("https://api.pokemontcg.io/v2/cards/" + cardId)
       .then((r) => r.json())
-      .then(data => {
-        console.log(data);
+      .then(card => {
+        console.log(card.data);
+        var new_url = card.data.images.small;
+        updateCardImage(new_url);
     });
-  } 
-    
+  }
+
+  const [cardImageURL, updateCardImage] = useState('https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png');
+
   return(
     <div>
       <h1>Hi this is the home component</h1>
-        <form onSubmit={handleSub}>
-          <input type="text" placeholder="Search for a Pokemon card"/>
-          <button type="submit">Search</button>
-        </form>
+        <SearchBar
+          placeholder="Search pokeman cards"
+          handleSub={handleSub}/>
+        <CardDisplay
+          cardImageURL={cardImageURL}/>
     </div>
     )
+}
+
+function SearchBar({
+  placeholder,
+  handleSub
+}) {
+  return (
+    <form onSubmit={handleSub}>
+      <input type="text" placeholder={placeholder}/>
+      <button type="submit">Search</button>
+    </form>
+  )
+}
+
+function CardDisplay({cardImageURL}) {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>
+              <h3>cardTitle</h3>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div width='245px' height='342px'>
+              <img width='245px' height='342px' src={cardImageURL} alt='pokemon card'/>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Home
--- server/app.py
+++ server/app.py
@@ -1,10 +1,8 @@
 from ipdb import set_trace
 from flask import Flask
-from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from sqlalchemy_serializer import SerializerMixin
 from models import db
-
 
 app = Flask(__name__)
 
@@ -12,11 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
-
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
